34e6585744dce5ecbca9624ce5d856a45ecd9094
Vitalii Venher <vengervn@gmail.com>
2024-07-28 18:17:07 +0300

    make build

---
+++ index.2344b175.js
@@ -0,0 +1,2 @@
+document.addEventListener("DOMContentLoaded",function(){var e=document.querySelector(".gallery__large-img");document.querySelectorAll(".list-item").forEach(function(t){t.addEventListener("click",function(t){t.preventDefault();var n=t.target.closest("a").href;e.src=n})})});
+//# sourceMappingURL=index.2344b175.js.map
---
+++ index.f75de5e1.js
@@ -0,0 +1,14 @@
+"use strict";
+document.addEventListener("DOMContentLoaded", ()=>{
+    const largeImg = document.querySelector(".gallery__large-img");
+    const galleryItems = document.querySelectorAll(".list-item");
+    galleryItems.forEach((item)=>{
+        item.addEventListener("click", (e)=>{
+            e.preventDefault();
+            const newSrc = e.target.closest("a").href;
+            largeImg.src = newSrc;
+        });
+    });
+});
+
+//# sourceMappingURL=index.f75de5e1.js.map
